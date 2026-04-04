77b6de0bf25088c7d90805ff51d6eabe68606402
natew <nathanpweir@gmail.com>
2026-04-04 12:51:43 -0400

    feat: Add ability to specify a time range to update movies and shows.

--- core/update_main.py
+++ core/update_main.py
@@ -13,7 +13,7 @@ if sys.version_info[0] >= 3:
 else:
 	import simplejson as jSon
 	from thread import start_new_thread, allocate_lock
-from datetime import datetime
+from datetime import datetime, timedelta
 from support.common import *
 from support.httptools import wait_for_internet
 from core.update_common import *
@@ -83,7 +83,12 @@ class Movies:
 		return
 
 	def getDBMovies(self):
-		jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetMovies","params":{"properties":["uniqueid","title"]},"id":1}'
+		dateAfter = 0
+		if UpdateTime > 0:
+			dateAfter = (datetime.now() - timedelta(days=UpdateTime)).strftime('%Y-%m-%d')
+			jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetMovies","params":{"properties":["uniqueid","title"], "filter": {"field": "dateadded", "operator": "greaterthan", "value": "' + str( dateAfter ) + '"}, "sort":{"method": "dateadded","order":"descending"}},"id":1}'
+		else:
+			jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetMovies","params":{"properties":["uniqueid","title"], "sort":{"method": "dateadded","order":"descending"}},"id":1}'
 		jSonResponse = xbmc.executeJSONRPC( jSonQuery )
 		jSonResponse = jSon.loads( jSonResponse )
 		try:
@@ -163,7 +168,11 @@ class TVShows:
 		return
 
 	def getDBTVShows(self):
-		jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetTVShows","params":{"properties":["uniqueid","title"]},"id":1}'
+		if UpdateTime > 0:
+			dateAfter = (datetime.now() - timedelta(days=UpdateTime)).strftime('%Y-%m-%d')
+			jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetTVShows","params":{"properties":["uniqueid","title"], "filter": {"field": "dateadded", "operator": "greaterthan", "value": "' + str( dateAfter ) + '"}, "sort":{"method": "dateadded","order":"descending"}},"id":1}'
+		else:
+			jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetTVShows","params":{"properties":["uniqueid","title"], "sort":{"method": "dateadded","order":"descending"}},"id":1}'
 		jSonResponse = xbmc.executeJSONRPC( jSonQuery )
 		jSonResponse = jSon.loads( jSonResponse )
 		try:
@@ -226,9 +235,13 @@ class TVShows:
 			progress.close()
 		return
 
-	def doUpdateSeasons(self, tvshowid, IMDb, progress, percentage):
+	def doUpdateSeasons(self, tvshowid, IMDb, progress, percentage, datefilter = True):
 		global num_threads
-		jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetSeasons","params":{"tvshowid":' + str( tvshowid ) + ',"properties":["season"]},"id":1}'
+		if datefilter == True and vUpdateTime > 0:
+			dateAfter = (datetime.now() - timedelta(days=UpdateTime)).strftime('%Y-%m-%d')
+			jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetSeasons","params":{"tvshowid":' + str( tvshowid ) + ',"properties":["season"], "filter": {"field": "dateadded", "operator": "greaterthan", "value": "' + str( dateAfter ) + '"}},"id":1}'
+		else:
+			jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetSeasons","params":{"tvshowid":' + str( tvshowid ) + ',"properties":["season"]},"id":1}'
 		jSonResponse = xbmc.executeJSONRPC( jSonQuery )
 		jSonResponse = jSon.loads( jSonResponse )
 		try:
@@ -243,12 +256,20 @@ class TVShows:
 		except: pass
 		return
 
-	def doUpdateEpisodes(self, tvshowid, tvshowTMDB, season, progress, percentage):
+	def doUpdateEpisodes(self, tvshowid, tvshowTMDB, season, progress, percentage, datefilter = True):
 		global num_threads
-		if season != -1:
-			jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetEpisodes","params":{"tvshowid":' + str( tvshowid ) + ', "season":' + str( season ) + ', "properties":["uniqueid","episode","season","showtitle"], "sort":{"method": "episode"}},"id":1}'
+		if season != -1 :
+			if datefilter == True and UpdateTime > 0:
+				dateAfter = (datetime.now() - timedelta(days=UpdateTime)).strftime('%Y-%m-%d')
+				jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetEpisodes","params":{"tvshowid":' + str( tvshowid ) + ', "season":' + str( season ) + ', "properties":["uniqueid","episode","season","showtitle"], "filter": {"field": "dateadded", "operator": "greaterthan", "value": "' + str( dateAfter ) + '"}, "sort":{"method": "episode"}},"id":1}'
+			else:
+				jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetEpisodes","params":{"tvshowid":' + str( tvshowid ) + ', "season":' + str( season ) + ', "properties":["uniqueid","episode","season","showtitle"], "sort":{"method": "episode"}},"id":1}'
 		else:
-			jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetEpisodes","params":{"tvshowid":' + str( tvshowid ) + ', "properties":["uniqueid","episode","season","showtitle"], "sort":{"method": "episode"}},"id":1}'
+			if datefilter == True and UpdateTime > 0:
+				dateAfter = (datetime.now() - timedelta(days=UpdateTime)).strftime('%Y-%m-%d')
+				jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetEpisodes","params":{"tvshowid":' + str( tvshowid ) + ', "properties":["uniqueid","episode","season","showtitle"], "filter": {"field": "dateadded", "operator": "greaterthan", "value": "' + str( dateAfter ) + '"}, "sort":{"method": "episode"}},"id":1}'
+			else:
+				jSonQuery = '{"jsonrpc":"2.0","method":"VideoLibrary.GetEpisodes","params":{"tvshowid":' + str( tvshowid ) + ', "properties":["uniqueid","episode","season","showtitle"], "sort":{"method": "episode"}},"id":1}'
 		jSonResponse = xbmc.executeJSONRPC( jSonQuery )
 		jSonResponse = jSon.loads( jSonResponse )
 		try:
--- support/common.py
+++ support/common.py
@@ -32,6 +32,16 @@ CompleteLog           = addonSettings.getSetting( "CompleteLog" )
 IncludeEpisodes       = addonSettings.getSetting( "IncludeEpisodes" )
 IncludeTop250         = addonSettings.getSetting( "IncludeTop250" )
 UpdateMode            = addonSettings.getSetting( "UpdateMode" )
+if (int(addonSettings.getSetting( "UpdateTime" )) == 1):
+    UpdateTime = 32
+elif (int(addonSettings.getSetting( "UpdateTime" )) == 2):
+    UpdateTime = 93
+elif (int(addonSettings.getSetting( "UpdateTime" )) == 3):
+    UpdateTime = 183
+elif (int(addonSettings.getSetting( "UpdateTime" )) == 4):
+    UpdateTime = 366
+else:
+    UpdateTime = 0
 IMDbDefault           = addonSettings.getSetting( "IMDbRatingDefault" )
 Sound                 = addonSettings.getSetting( "NotificationsSound" )
 ScheduleEnabled       = addonSettings.getSetting( "ScheduleEnabled" )
